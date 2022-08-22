bda1f4b846a0c3d9e6b1569ee6227f07af8c7249
ErikKalkoken <erik.kalkoken@gmail.com>
2022-08-23 01:46:16 +0200

    Replace admin action with faster one

--- taskanalytics/admin.py
+++ taskanalytics/admin.py
@@ -9,9 +9,22 @@ class TaskLogAdmin(admin.ModelAdmin):
     ordering = ["-timestamp"]
     list_filter = ("state", "timestamp", "app_name", "task_name")
     search_fields = ("task_name", "app_name", "task_id")
+    actions = ["delete_selected_2"]
 
     def has_add_permission(self, *args, **kwargs) -> bool:
         return False
 
     def has_change_permission(self, *args, **kwargs):
         return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
+
+    @admin.action(description="Delete selected entries (NO CONFIRMATION!")
+    def delete_selected_2(self, request, queryset):
+        entries_count = queryset.count()
+        queryset._raw_delete(queryset.db)
+        self.message_user(request, f"Deleted {entries_count} entries.")

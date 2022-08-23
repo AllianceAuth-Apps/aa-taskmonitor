caf9e3a0f92217b6cd1b093a712bfefd951e7567
ErikKalkoken <erik.kalkoken@gmail.com>
2022-08-23 04:02:32 +0200

    Fix pre-commit issue

--- taskanalytics/migrations/0003_alter_tasklogentry_runtime.py
+++ taskanalytics/migrations/0003_alter_tasklogentry_runtime.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('taskanalytics', '0002_tasklogentry_delete_tasklog'),
+        ("taskanalytics", "0002_tasklogentry_delete_tasklog"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='tasklogentry',
-            name='runtime',
+            model_name="tasklogentry",
+            name="runtime",
             field=models.FloatField(db_index=True, default=None, null=True),
         ),
     ]
